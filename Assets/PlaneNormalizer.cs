a60ab2f4cc2d6c19be251de1292120f8326fa053
Solus <yannis2004@live.com>
2022-09-26 15:16:02 -0500

    Updated File.

--- Assets/PlaneNormalizer.cs
+++ Assets/PlaneNormalizer.cs
@@ -6,10 +6,12 @@ public class PlaneNormalizer : MonoBehaviour
 {
     [Tooltip("Drag and drop a new PLANE game object. This is your 'base' that you will be building on top of.")]
     public GameObject plane;
-
+    float planeSize = 5f;
     private void OnDrawGizmos()
     {
-        plane.transform.localScale = new Vector3(10, 10, 10);
+        plane.transform.localScale = new Vector3(planeSize, planeSize, planeSize);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(plane.transform.position, Vector3.up * planeSize + plane.transform.position);
     }
 
 }
