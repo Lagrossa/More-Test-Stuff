085228cf21a145d0528b3992eb5287c2758afa8b
Solus <yannis2004@live.com>
2022-09-26 15:30:35 -0500

    Update PlaneNormalizer.cs

--- Assets/PlaneNormalizer.cs
+++ Assets/PlaneNormalizer.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEditor;
 public class PlaneNormalizer : MonoBehaviour
 {
-    [Tooltip("Drag and drop a new PLANE game object. This is your 'base' that you will be building on top of.")]
+    [Header("Drag and drop a new PLANE game object.")]
+    [Tooltip("This is your 'base' that you will be building on top of.")]
     public GameObject plane;
-    float planeSize = 5f;
+    [Header("Keep at 5 unless Lain or Louanne tell you otherwise.")]
+    public float planeSize = 5f;
+
     private void OnDrawGizmos()
     {
         plane.transform.localScale = new Vector3(planeSize, planeSize, planeSize);
-        Gizmos.color = Color.red;
-        Gizmos.DrawLine(plane.transform.position, Vector3.up * planeSize + plane.transform.position);
+
+        //Plane visualizations
+        Gizmos.color = Color.green;
+        Vector3 planeHeight = (Vector3.up * planeSize + plane.transform.position);
+        Gizmos.DrawLine(plane.transform.position, planeHeight);
+        Handles.color = Color.green;
+        Handles.Label(new Vector3(planeHeight.x, planeHeight.y/2, planeHeight.z),
+            planeSize + "m tall");
+        Handles.color = Color.blue;
     }
 
 }
