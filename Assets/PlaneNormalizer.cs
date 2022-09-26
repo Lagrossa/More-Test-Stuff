e28b87c8585a9ab316d0d8f847129e5cd32380bd
Solus <yannis2004@live.com>
2022-09-26 15:49:53 -0500

    Update PlaneNormalizer.cs

--- Assets/PlaneNormalizer.cs
+++ Assets/PlaneNormalizer.cs
@@ -10,7 +10,7 @@ public class PlaneNormalizer : MonoBehaviour
     [Header("Keep at 5 unless Lain or Louanne tell you otherwise.")]
     public float planeSize = 5f;
 
-    float basePlane = 5f; // L/W/H of a plane in its 'raw' unedited form.
+    const float basePlane = 5f; // L/W/H of a plane in its 'raw' unedited form. DO NOT CHANGE
     private void OnDrawGizmos()
     {
         //normalize to planesize
@@ -19,9 +19,9 @@ public class PlaneNormalizer : MonoBehaviour
         //Plane visualizations
         // y Axis
         Gizmos.color = Color.green;
-        Vector3 planeHeight = Vector3.up * planeSize + plane.transform.position;
-        Gizmos.DrawLine(plane.transform.position, planeHeight);
-        Handles.Label(new Vector3(planeHeight.x, planeHeight.y/2, planeHeight.z),
+        Vector3 planeHeight = Vector3.up * planeSize;
+        Gizmos.DrawLine(plane.transform.position, planeHeight + plane.transform.position);
+        Handles.Label(new Vector3(planeHeight.x, planeHeight.y/2, planeHeight.z) + plane.transform.position,
             planeSize + "m tall");
         // x Axis
         Gizmos.color = Color.red;
@@ -33,7 +33,7 @@ public class PlaneNormalizer : MonoBehaviour
         Gizmos.color = Color.blue;
         Vector3 planeWidth = Vector3.forward * planeSize;
         Gizmos.DrawLine(plane.transform.position, planeWidth + plane.transform.position);
-        Handles.Label(new Vector3(planeWidth.x/2, planeWidth.y, planeWidth.z/2) + plane.transform.position,
+        Handles.Label(new Vector3(planeWidth.x, planeWidth.y, planeWidth.z/2) + plane.transform.position,
             planeSize + "m wide");
     }
 
