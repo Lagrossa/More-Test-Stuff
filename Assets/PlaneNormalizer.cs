f785b52167eb8c18c3c9b2420279734485b1e170
Solus <yannis2004@live.com>
2022-09-26 16:03:53 -0500

    Update PlaneNormalizer.cs

--- Assets/PlaneNormalizer.cs
+++ Assets/PlaneNormalizer.cs
@@ -9,32 +9,53 @@ public class PlaneNormalizer : MonoBehaviour
     public GameObject plane;
     [Header("Keep at 5 unless Lain or Louanne tell you otherwise.")]
     public float planeSize = 5f;
+    [Header("On if you want visual guides on. Off if not.")]
+    public bool visualGuides;
+    const float basePlane = 5f; // L/W/H of a plane in its 'raw' unedited form. DO NOT CHANGE.
 
-    const float basePlane = 5f; // L/W/H of a plane in its 'raw' unedited form. DO NOT CHANGE
+    [Header("Your 'node' sequence will determine where you connect. Enter exactly.")]
+    public int node1;
+    public int node2;
+    public int node3;
+    public int node4;
+
+    public float nodeRadius = .5f;
     private void OnDrawGizmos()
     {
-        //normalize to planesize
+        //normalize to plane size
         plane.transform.localScale = new Vector3(planeSize/ basePlane, planeSize/ basePlane, planeSize/ basePlane);
 
         //Plane visualizations
-        // y Axis
-        Gizmos.color = Color.green;
-        Vector3 planeHeight = Vector3.up * planeSize;
-        Gizmos.DrawLine(plane.transform.position, planeHeight + plane.transform.position);
-        Handles.Label(new Vector3(planeHeight.x, planeHeight.y/2, planeHeight.z) + plane.transform.position,
-            planeSize + "m tall");
-        // x Axis
-        Gizmos.color = Color.red;
-        Vector3 planeLength = Vector3.right * planeSize;
-        Gizmos.DrawLine(plane.transform.position, planeLength + plane.transform.position);
-        Handles.Label(new Vector3(planeLength.x/2, planeLength.y, planeLength.z) + plane.transform.position,
-            planeSize + "m long");
-        // z Axis
-        Gizmos.color = Color.blue;
-        Vector3 planeWidth = Vector3.forward * planeSize;
-        Gizmos.DrawLine(plane.transform.position, planeWidth + plane.transform.position);
-        Handles.Label(new Vector3(planeWidth.x, planeWidth.y, planeWidth.z/2) + plane.transform.position,
-            planeSize + "m wide");
+        if (visualGuides)
+        {
+            // y Axis
+            Gizmos.color = Color.green;
+            Vector3 planeHeight = Vector3.up * planeSize;
+            Gizmos.DrawLine(plane.transform.position, planeHeight + plane.transform.position);
+            Handles.Label(new Vector3(planeHeight.x, planeHeight.y / 2, planeHeight.z) + plane.transform.position,
+                planeSize + "m tall");
+            // x Axis
+            Gizmos.color = Color.red;
+            Vector3 planeLength = Vector3.right * planeSize;
+            Gizmos.DrawLine(plane.transform.position, planeLength + plane.transform.position);
+            Handles.Label(new Vector3(planeLength.x / 2, planeLength.y, planeLength.z) + plane.transform.position,
+                planeSize + "m long");
+            // z Axis
+            Gizmos.color = Color.blue;
+            Vector3 planeWidth = Vector3.forward * planeSize;
+            Gizmos.DrawLine(plane.transform.position, planeWidth + plane.transform.position);
+            Handles.Label(new Vector3(planeWidth.x, planeWidth.y, planeWidth.z / 2) + plane.transform.position,
+                planeSize + "m wide");
+
+            //Connection Nodes
+            Vector3 nodeV1; //+w,
+            Vector3 nodeV2;
+            Vector3 nodeV3;
+            Vector3 nodeV4;
+
+            // Node 1
+            Gizmos.DrawSphere(planeWidth+plane.transform.position, nodeRadius);
+        }
     }
 
 }
