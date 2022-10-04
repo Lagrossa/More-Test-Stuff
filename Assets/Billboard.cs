a61332ce7277ae3776eeb02843792dadf67b3ff4
Solus <yannis2004@live.com>
2022-10-04 09:45:04 -0500

    Updated Files.

--- Assets/Billboard.cs
+++ Assets/Billboard.cs
@@ -8,16 +8,17 @@ public class Billboard : MonoBehaviour
     Vector3 dirToObject;
     void Update()
     {
-        dirToObject = LookAtObj.transform.position - transform.position;
-        Quaternion rot = Quaternion.LookRotation(dirToObject);
-        transform.rotation = Quaternion.Slerp(transform.rotation, rot, 1 * Time.deltaTime);
     }
 
     private void OnDrawGizmos()
     {
+        dirToObject = LookAtObj.transform.position - transform.position;
         Gizmos.color = Color.cyan;
         Gizmos.DrawLine(transform.position, dirToObject + transform.position);
+        //Check
         Gizmos.color = Color.red;
-        Gizmos.DrawLine(transform.position, LookAtObj.position);
+        //Gizmos.DrawLine(transform.position, LookAtObj.position);
+        Quaternion rot = Quaternion.LookRotation(dirToObject);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rot, 1 * Time.deltaTime);
     }
 }
