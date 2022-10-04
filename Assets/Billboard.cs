3c7de083f62430905bcbc8243bd30dcb595c047c
Solus <yannis2004@live.com>
2022-10-04 09:41:20 -0500

    Update Billboard.cs

--- Assets/Billboard.cs
+++ Assets/Billboard.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class Billboard : MonoBehaviour
 {
     public Transform LookAtObj;
-
+    Vector3 dirToObject;
     void Update()
     {
-        Vector3 dirToObject = LookAtObj.transform.position - transform.position;
+        dirToObject = LookAtObj.transform.position - transform.position;
         Quaternion rot = Quaternion.LookRotation(dirToObject);
         transform.rotation = Quaternion.Slerp(transform.rotation, rot, 1 * Time.deltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawLine(transform.position, dirToObject + transform.position);
     }
 }
