ac08092a8a4b4a0a22cf5e936bc513a056c7e7ea
Solus <yannis2004@live.com>
2022-10-14 15:36:41 -0500

    Updated Files.

--- Assets/Trigonometry.cs
+++ Assets/Trigonometry.cs
@@ -9,6 +9,7 @@ public class Trigonometry : MonoBehaviour
     public float TAU = 6.28f; // Divide by X to mimic Unit Circle
     public int dots = 16;
     public float radius = 1;
+    public float thickness = 1f;
     public float z;
     [Range(0,360)]
     public float angThreshold;
@@ -20,13 +21,13 @@ public class Trigonometry : MonoBehaviour
             float angRadians = TAU * t;
             float xVal = Mathf.Cos(angRadians);
             float yVal = Mathf.Sin(angRadians);
-
+            Vector3 triggerTransform = trigger.transform.position;
             // TESTING
             //float zVal = Mathf.Tan(angRadians);
             //if (Mathf.Abs(zVal) > 1)
             //    zVal /= zVal;
             Vector3 point = new Vector3(xVal, 0, yVal);
-            Handles.DrawWireArc(transform.position, Vector3.forward, (transform.position- trigger.transform.position).normalized, angThreshold, radius);
+            Handles.DrawWireArc(transform.position, Vector3.up, (triggerTransform - transform.position).normalized, -angThreshold, radius, thickness); ;
             if (angRadians < Mathf.Deg2Rad * angThreshold)
             {
                 Gizmos.color = Color.cyan;
@@ -37,6 +38,12 @@ public class Trigonometry : MonoBehaviour
             }
             //Gizmos.DrawSphere(point, radius);
             Gizmos.DrawLine(transform.position, point);
+
+
+            //Draw to sphere color determined by dotproduct
+            float dotProduct = Vector3.Dot(transform.position, triggerTransform);
+            Gizmos.color = (dotProduct == 1 ? Color.green : Color.red);
+            Gizmos.DrawLine(transform.position, transform.position + triggerTransform);
 
         }
         
