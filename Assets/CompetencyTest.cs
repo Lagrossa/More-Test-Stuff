5d40e5ed72e80e0d5d1525e558583647186cebd5
Solus <yannis2004@live.com>
2022-10-20 08:55:36 -0500

    Update CompetencyTest.cs

--- Assets/CompetencyTest.cs
+++ Assets/CompetencyTest.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class CompetencyTest : MonoBehaviour
 {
+    public int dots;
+    const float TAU = 6.28f;
     private void OnDrawGizmos()
     {
         for(int x = 0; x < dots; x++)
         {
-
+            float percentCircumference = (float)x / dots;
+            float degRad = percentCircumference * TAU;
+            Vector3 point = new Vector3(Mathf.Cos(degRad), Mathf.Sin(degRad), 0);
         }
 }
